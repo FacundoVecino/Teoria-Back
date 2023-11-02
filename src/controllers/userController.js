ab678958a3c5b2bbb9c349355c17b85c0369e6dc
FacundoVecino <facundovecino32@gmail.com>
2023-11-02 11:47:39 -0300

    Agrego GET y POST

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -11,3 +11,31 @@ export const getUsers = async (_, res) => {
     });
   }
 };
+
+export const postUser = async (req, res) => {
+  const { body } = req;
+  const newUser = new UserCollection({
+    firstname: body.firstname,
+    lastname: body.lastname,
+    username: body.username,
+    password: body.password,
+  });
+
+  try {
+    await newUser.save();
+    res.status(201).json({
+      message: 'Usuario creado exitosamente',
+    });
+  } catch (e) {
+    if (e.message.includes('duplicate')) {
+      res.status(400).json({
+        message: 'El nombre de usuario ya está en uso',
+      });
+    } else {
+      res.status(500).json({
+        message: 'Ocurrió un error guardando el usuario',
+        error: e.message,
+      });
+    }
+  }
+};
