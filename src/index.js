b22c0e46033678096895430ad0c2959fb856982b
FacundoVecino <facundovecino32@gmail.com>
2023-11-01 21:03:23 -0300

    Configuro Express

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
-console.log("Hola mundo desde el backend")
+import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+
+// 1. Iniciar la aplicacion
+const app = express();
+
+// 2. Configuraciones generales
+const PORT = process.env.PORT || 5000;
+
+// 3. Middlewares
+app.use(morgan("dev")); // Genera console.log que brindan informacion cada vez que se conectan al servidor
+
+//CROSS-ORIGIN-RESOURCE-SHARING
+app.use(cors()) // (Modo puta) 
+/* app.use(cors({
+    origin: "https://rollingcode.com"   Sirve para especificar la conexion entre distintas url
+})) */
+
+app.use(express.json()) // Le dice a la aplicacion que se prepare para recibir en formato json
+
+// 4. Rutas
+
+// 5. Iniciar el loop del servidor
+app.listen(PORT, () => {
+    console.log(`Servidor ejecutandose en el puerto ${PORT}`)
+});
